e36c940c6dccd8e8a99012799f51f2827c7e0873
skozawa <argentcrescent@gmail.com>
2012-02-03 19:37:42 +0900

    change BubbleSort

--- lib/Sorter/BubbleSort.pm
+++ lib/Sorter/BubbleSort.pm
@@ -10,7 +10,7 @@ sub sort {
     for my $i ( 0 .. $#{$this->{values}}-1 ) {
 	for my $j ( 0 .. $#{$this->{values}}-$i-1 ) {
 	    if(${$this->{values}}[$j] > ${$this->{values}}[$j+1]) {
-		$this->swap($i, $j);
+		$this->swap($j, $j+1);
 	    }
 	}
     }
